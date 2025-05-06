2864e70dc53df474530637983eb6b375758ba16b
Asahel-Mariquit <amariquit1@byupathway.edu>
2025-05-06 21:36:23 +0800

    Finished Week 1 Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Give your grade percentage: ");
+        string userGradePercentage = Console.ReadLine();
+
+        int convertedUserGradePercentage = int.Parse(userGradePercentage);
+
+        string letter = "";
+        if (convertedUserGradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (convertedUserGradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (convertedUserGradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (convertedUserGradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (convertedUserGradePercentage <= 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Please input a valid number");
+        }
+
+        string sign = "";
+        int lastDigit = convertedUserGradePercentage % 10;
+
+        if (lastDigit >= 7 && (letter != "A" && letter != "F"))
+        {
+            sign = "+";
+        }
+        else if (lastDigit <= 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        { Console.WriteLine($"Your grade is {letter}{sign}"); }
+
+        if (convertedUserGradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Almost there. Please try again next time.");
+        }
+
+
     }
 }
