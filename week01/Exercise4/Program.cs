1f24de99f0297f6a3a1ec0bd8c5571f3b5e87714
Asahel-Mariquit <amariquit1@byupathway.edu>
2025-05-07 22:26:50 +0800

    Finished Week 1 Exercise 3 Core Requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string userInput = "";
+        int sum = 0;
+        int numberSize = 0;
+        int average = 0;
+        int maximum = -1;
+        List<int> numbers;
+        while (userInput != "0")
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            userInput = Console.ReadLine();
+            int convertedNumber = int.Parse(userInput);
+
+            numbers = new List<int>();
+
+            if (convertedNumber != 0)
+            {
+                numbers.Add(convertedNumber);
+            }
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+                if (number >= maximum)
+                {
+                    maximum = number;
+                }
+            }
+
+            numberSize += numbers.Count;
+            average = sum / numberSize;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
+        
     }
 }
