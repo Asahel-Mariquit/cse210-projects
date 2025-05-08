0dd32ad6b7a0a5d5ba8c6b25b64cf129af97fe21
Asahel-Mariquit <amariquit1@byupathway.edu>
2025-05-08 17:01:54 +0800

    Finished Week 1 Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,40 +5,58 @@ class Program
 {
     static void Main(string[] args)
     {
+        // initialize variables
         string userInput = "";
-        int sum = 0;
+        float sum = 0;
         int numberSize = 0;
-        int average = 0;
-        int maximum = -1;
-        List<int> numbers;
+        int largest = -1;
+        int smallest = 999999999;
+        // Create list named numbers
+        List<int> numbers = new List<int>();
+
+        // Continuee loop until user input is 0
         while (userInput != "0")
         {
             Console.Write("Enter a list of numbers, type 0 when finished. ");
             userInput = Console.ReadLine();
             int convertedNumber = int.Parse(userInput);
 
-            numbers = new List<int>();
-
             if (convertedNumber != 0)
             {
                 numbers.Add(convertedNumber);
             }
-
-            foreach (int number in numbers)
-            {
-                sum += number;
-                if (number >= maximum)
-                {
-                    maximum = number;
-                }
-            }
-
-            numberSize += numbers.Count;
-            average = sum / numberSize;
         }
+        // loop through list to get the sum, largest numbers, and smalest number
+        foreach (int number in numbers)
+        {
+            sum += number;
+            // Find the maximum or largest number
+            if (number >= largest)
+            {
+                largest = number;
+            }
+            // Find the smallest number
+            if (number <= smallest)
+            {
+                smallest = number;
+            }
+        }
+        // get list count
+        numberSize += numbers.Count;
+        // compute average
+        float average = sum / numberSize;
+        // Print sum, average, largest number, and smallest number
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {average}");
-        Console.WriteLine($"The largest number is: {maximum}");
-        
+        Console.WriteLine($"The largest number is: {largest}");
+        Console.WriteLine($"The smallest positive number is: {smallest}");
+        // Sort list
+        numbers.Sort();
+        // Loop and print sorted list
+        Console.WriteLine("The sorted list is: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
