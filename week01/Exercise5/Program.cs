6f9cbf6d520ae348e8033da350530d9c3bdd3dcb
Asahel-Mariquit <amariquit1@byupathway.edu>
2025-05-08 11:19:22 +0800

    Finished Week 1 Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName("Please enter your name: ");
+        int userNumber = PromptUserNumber("Please enter you favorite number: ");
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName(string prompt)
+        {
+            Console.Write(prompt);
+            string userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int PromptUserNumber(string prompt)
+        {
+            Console.Write(prompt);
+            string userNumber = Console.ReadLine();
+            int number = int.Parse(userNumber);
+
+            return number;
+        }
+
+        static int SquareNumber(int userNumber)
+        {
+            int squaredNumber = userNumber * userNumber;
+
+            return squaredNumber;
+        }
+
+        void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"{userName}, the square of your favorite number is {squaredNumber}");
+        }
     }
 }
